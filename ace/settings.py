5b9f46eb04d16dc3d65870ca2c9bb3a73c41a22c
Willow Celeste <willow@punkave.com>
2022-03-15 10:32:35 -0400

    adjusts settings.py installed apps

--- ace/settings.py
+++ ace/settings.py
@@ -37,8 +37,8 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'import_export',
     'coasters',
+    'import_export'
 ]
 
 MIDDLEWARE = [
